2d7d5245b2418a1b199e33eab2b8478b188b0c72
Natasha S Love <debora@inbox.ru>
2018-03-07 20:24:29 -0600

    ifstream

--- CIS2013_Week08_Lab02.cpp
+++ CIS2013_Week08_Lab02.cpp
@@ -4,8 +4,11 @@
 using namespace std;
 
 int main () {
-	int number=0;
+	int number;
+	int one, two, three, four, five;
 	char cont = 'y';
+	
+	// Inputs/ prints ready numbers from the file
 	ifstream in_a;
 	
 	in_a.open ("numbers.dat");
@@ -13,10 +16,13 @@ int main () {
 		cout << "Something went wrong with numbers.dat";
 		exit(1);
 	}
+	// Puts out numbers from the file (in which numbers already are situated) 
 	in_a >> one >> two >> three >> four;
-	cout << "Our previous numbers are " << one << " and ";
-	cout << two << " and " << three << " not to forget ";
-	cout << four;
+	in_a >> five;
+	
+	cout << "Add our previous numbers from " << one << " to " << five << endl;
+	cout << "Numbers total to: " << (one + two + three + four) << endl;
+	cout << "And our character is:" << five<< endl;
 	
 	in_a.close();
 	return 0;
